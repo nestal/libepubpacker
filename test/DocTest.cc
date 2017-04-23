b0769b000950e78fceb79e5e3140c65bb9be5f13
Nestal Wan <me@nestal.net>
2017-04-23 11:10:55 +0800

    updated license information

--- test/DocTest.cc
+++ test/DocTest.cc
@@ -1,9 +1,9 @@
 /*
 	Copyright © 2017 Wan Wai Ho <me@nestal.net>
     
-    This file is subject to the terms and conditions of the GNU General Public
-    License.  See the file COPYING in the main directory of the booksrv
-    distribution for more details.
+    This file is subject to the terms and conditions of the Library GNU
+    General Public License.  See the file COPYING in the main directory
+    of the libepubpacker distribution for more details.
 */
 
 //
