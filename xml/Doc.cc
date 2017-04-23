0955fcce08b17206ac08cd1302563279cb7adf46
Nestal Wan <me@nestal.net>
2017-04-23 10:40:19 +0800

    initial CMakeLists for libepubpacker

---
+++ xml/Doc.cc
@@ -0,0 +1,18 @@
+/*
+	Copyright © 2017 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the booksrv
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 4/23/17.
+//
+
+#include "Doc.hh"
+
+namespace xml {
+
+
+}
---
+++ xml/Doc.hh
@@ -0,0 +1,22 @@
+/*
+	Copyright © 2017 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the booksrv
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 4/23/17.
+//
+
+#pragma once
+
+namespace xml {
+
+class Doc
+{
+
+};
+
+} // end of namespace
